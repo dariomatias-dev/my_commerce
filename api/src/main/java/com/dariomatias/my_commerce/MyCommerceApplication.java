af20c43f40b24a34fe5c6670952847c1b94ee46b
dariomatias-dev <matiasdario75@gmail.com>
2025-10-07 16:29:28 -0300

    feat(api): initialized backend
    - Backend project initialized.

---
+++ api/src/main/java/com/dariomatias/my_commerce/MyCommerceApplication.java
@@ -0,0 +1,13 @@
+package com.dariomatias.my_commerce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyCommerceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyCommerceApplication.class, args);
+	}
+
+}
---
+++ api/src/test/java/com/dariomatias/my_commerce/MyCommerceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dariomatias.my_commerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyCommerceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
