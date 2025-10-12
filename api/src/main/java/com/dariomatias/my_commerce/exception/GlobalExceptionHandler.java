91ea8338f128c20a92f9a02cd05170ea39836447
dariomatias-dev <matiasdario75@gmail.com>
2025-10-12 19:25:31 -0300

    feat(api/src/main/java/com/dariomatias/my_commerce/exception/GlobalExceptionHandler.java): restructure error handling
    - Refactored the class to properly handle application errors, including unforeseen ones.

--- api/src/main/java/com/dariomatias/my_commerce/exception/GlobalExceptionHandler.java
+++ api/src/main/java/com/dariomatias/my_commerce/exception/GlobalExceptionHandler.java
@@ -2,31 +2,33 @@ package com.dariomatias.my_commerce.exception;
 
 import com.dariomatias.my_commerce.dto.ApiResponse;
 import com.dariomatias.my_commerce.dto.FieldError;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MethodArgumentNotValidException;
-import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
 
-import java.util.List;
 import java.util.stream.Collectors;
 
-@ControllerAdvice
+@RestControllerAdvice
 public class GlobalExceptionHandler {
 
-    @ExceptionHandler(MethodArgumentNotValidException.class)
-    public ResponseEntity<ApiResponse<Object>> handleValidationExceptions(MethodArgumentNotValidException ex) {
+    @ExceptionHandler(Exception.class)
+    public ApiResponse<String> handleException(Exception ex) {
+        return ApiResponse.error(500, "Ocorreu um erro interno: " + ex.getMessage());
+    }
 
-        List<FieldError> errors = ex.getBindingResult()
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ApiResponse<?> handleValidationException(MethodArgumentNotValidException ex) {
+        var fieldErrors = ex.getBindingResult()
                 .getFieldErrors()
                 .stream()
-                .map(err -> new FieldError(err.getField(), err.getDefaultMessage()))
+                .map(error -> new FieldError(error.getField(), error.getDefaultMessage()))
                 .collect(Collectors.toList());
 
-        ApiResponse<Object> response = ApiResponse.error(HttpStatus.BAD_REQUEST.value(),
-                "Erro de validação",
-                errors);
+        return ApiResponse.error(400, "Erro de validação", fieldErrors);
+    }
 
-        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(response);
+    @ExceptionHandler(RuntimeException.class)
+    public ApiResponse<String> handleRuntimeException(RuntimeException ex) {
+        return ApiResponse.error(400, ex.getMessage());
     }
 }
