15c4296ca7686bb377b53f7a0749926edb0a64a9
dariomatias-dev <matiasdario75@gmail.com>
2025-10-15 15:27:20 -0300

    feat(api/src/main/java/com/dariomatias/my_commerce/exception/GlobalExceptionHandler.java): update to handle more error cases
    - Updated to handle `ResponseStatusException` and `Exception` types.

--- api/src/main/java/com/dariomatias/my_commerce/exception/GlobalExceptionHandler.java
+++ api/src/main/java/com/dariomatias/my_commerce/exception/GlobalExceptionHandler.java
@@ -5,15 +5,16 @@ import com.dariomatias.my_commerce.dto.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.stream.Collectors;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
 
-    @ExceptionHandler(Exception.class)
-    public ApiResponse<String> handleException(Exception ex) {
-        return ApiResponse.error(500, "Ocorreu um erro interno: " + ex.getMessage());
+    @ExceptionHandler(ResponseStatusException.class)
+    public ApiResponse<String> handleResponseStatusException(ResponseStatusException ex) {
+        return ApiResponse.error(ex.getStatusCode().value(), ex.getReason());
     }
 
     @ExceptionHandler(MethodArgumentNotValidException.class)
@@ -30,5 +31,10 @@ public class GlobalExceptionHandler {
     @ExceptionHandler(RuntimeException.class)
     public ApiResponse<String> handleRuntimeException(RuntimeException ex) {
         return ApiResponse.error(400, ex.getMessage());
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ApiResponse<String> handleException(Exception ex) {
+        return ApiResponse.error(500, "Ocorreu um erro interno: " + ex.getMessage());
     }
 }
