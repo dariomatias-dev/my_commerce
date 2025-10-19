69a9bc04e6534d299e18ba04b9ada13de3170985
dariomatias-dev <matiasdario75@gmail.com>
2025-10-19 10:44:54 -0300

    feat(api/src/main/java/com/dariomatias/my_commerce/model/Category.java): add `Getter` and `Setter` annotations
    - Added `Getter` and `Setter` annotations to automatically generate methods for class fields.

--- api/src/main/java/com/dariomatias/my_commerce/model/Category.java
+++ api/src/main/java/com/dariomatias/my_commerce/model/Category.java
@@ -1,6 +1,7 @@
 package com.dariomatias.my_commerce.model;
 
 import jakarta.persistence.*;
+import lombok.Getter;
 import lombok.Setter;
 import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.UpdateTimestamp;
@@ -11,47 +12,47 @@ import java.util.UUID;
 @Table(name = "categories")
 public class Category {
 
+    @Getter
     @Setter
     @Id
-    @GeneratedValue
+    @GeneratedValue(generator = "UUID")
+    @org.hibernate.annotations.GenericGenerator(
+            name = "UUID",
+            strategy = "org.hibernate.id.UUIDGenerator"
+    )
+    @Column(updatable = false, nullable = false)
     private UUID id;
 
+    @Getter
     @Setter
     @ManyToOne(optional = false)
     @JoinColumn(name = "store_id")
     private Store store;
 
+    @Getter
     @Setter
     @Column(nullable = false)
     private String name;
 
+    @Getter
+    @Setter
     @CreationTimestamp
     @Column(nullable = false, updatable = false)
     private LocalDateTime createdAt;
 
+    @Getter
+    @Setter
     @UpdateTimestamp
     @Column(nullable = false)
     private LocalDateTime updatedAt;
 
+    @Setter
+    @Transient
+    private UUID storeId;
+
     public Category() {}
 
-    public UUID getId() {
-        return id;
-    }
-
-    public Store getStore() {
-        return store;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public LocalDateTime getCreatedAt() {
-        return createdAt;
-    }
-
-    public LocalDateTime getUpdatedAt() {
-        return updatedAt;
+    public UUID getStoreId() {
+        return store != null ? store.getId() : storeId;
     }
 }
