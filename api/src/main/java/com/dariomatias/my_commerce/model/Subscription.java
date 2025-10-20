715ea867a7e38a278271acc9bf7071150b3058de
dariomatias-dev <matiasdario75@gmail.com>
2025-10-20 12:22:26 -0300

    feat(api/src/main/java/com/dariomatias/my_commerce/model/Subscription.java): added `Setter` annotations and unmapped database fields
    - Added `Setter` annotation to `createdAt` and `updatedAt`.
    - Added `userId` and `planId` properties not mapped to the database.

--- api/src/main/java/com/dariomatias/my_commerce/model/Subscription.java
+++ api/src/main/java/com/dariomatias/my_commerce/model/Subscription.java
@@ -47,14 +47,32 @@ public class Subscription {
     private Boolean isActive;
 
     @Getter
+    @Setter
     @CreationTimestamp
     @Column(nullable = false, updatable = false)
     private LocalDateTime createdAt;
 
     @Getter
+    @Setter
     @UpdateTimestamp
     @Column(nullable = false)
     private LocalDateTime updatedAt;
 
+    @Setter
+    @Transient
+    private UUID userId;
+
+    @Setter
+    @Transient
+    private UUID planId;
+
     public Subscription() {}
+
+    public UUID getUserId() {
+        return user != null ? user.getId() : userId;
+    }
+
+    public UUID getPlanId() {
+        return plan != null ? plan.getId() : planId;
+    }
 }
