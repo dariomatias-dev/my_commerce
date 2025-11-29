553e1801963b03f28f548860caf875f7839b4fb7
dariomatias-dev <matiasdario75@gmail.com>
2025-11-29 14:30:49 -0300

    feat(api/src/main/java/com/dariomatias/my_commerce/config): created bean for `PasswordEncoder`
    - Moved `PasswordEncoder` bean configuration from `SecurityConfig` to its own independent file.

--- api/src/main/java/com/dariomatias/my_commerce/config/SecurityConfig.java
+++ api/src/main/java/com/dariomatias/my_commerce/config/SecurityConfig.java
@@ -4,10 +4,9 @@ import com.dariomatias.my_commerce.security.JwtAuthenticationEntryPoint;
 import com.dariomatias.my_commerce.security.JwtAuthenticationFilter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
@@ -23,11 +22,6 @@ public class SecurityConfig {
                           JwtAuthenticationEntryPoint jwtAuthenticationEntryPoint) {
         this.jwtAuthenticationFilter = jwtAuthenticationFilter;
         this.jwtAuthenticationEntryPoint = jwtAuthenticationEntryPoint;
-    }
-
-    @Bean
-    public PasswordEncoder passwordEncoder() {
-        return new BCryptPasswordEncoder();
     }
 
     @Bean
