e2cc071649569a9a5958ab5f9de7d679ffb6a02f
dariomatias-dev <matiasdario75@gmail.com>
2025-12-21 14:13:24 -0300

    refactor(web/src/hooks/use-auth.ts): simplify methods
    - Remove unnecessary use of `async`.

--- web/src/hooks/use-auth.ts
+++ web/src/hooks/use-auth.ts
@@ -1,3 +1,5 @@
+"use client";
+
 import { AuthResponse } from "@/@types/auth/auth-response";
 import { LoginRequest } from "@/@types/auth/login-request";
 import { RecoverPasswordRequest } from "@/@types/auth/recover-password-request";
@@ -9,35 +11,28 @@ import { VerifyEmailRequest } from "@/@types/auth/verify-email-request";
 import { useApi } from "./use-api";
 
 export const useAuth = () => {
-  const api = useApi();
+  const apiInstance = useApi();
 
-  const login = async (data: LoginRequest) => {
-    return api.post<AuthResponse>("/auth/login", data);
-  };
+  const login = (data: LoginRequest) =>
+    apiInstance.post<AuthResponse>("/auth/login", data);
 
-  const signup = async (data: SignupRequest) => {
-    return api.post<void>("/auth/signup", data);
-  };
+  const signup = (data: SignupRequest) =>
+    apiInstance.post<void>("/auth/signup", data);
 
-  const verifyEmail = async (data: VerifyEmailRequest) => {
-    return api.post<void>("/auth/verify-email", data);
-  };
+  const verifyEmail = (data: VerifyEmailRequest) =>
+    apiInstance.post<void>("/auth/verify-email", data);
 
-  const resendVerificationEmail = async (data: ResendEmailRequest) => {
-    return api.post<void>("/auth/resend-verification-email", data);
-  };
+  const resendVerificationEmail = (data: ResendEmailRequest) =>
+    apiInstance.post<void>("/auth/resend-verification-email", data);
 
-  const recoverPassword = async (data: RecoverPasswordRequest) => {
-    return api.post<void>("/auth/recover-password", data);
-  };
+  const recoverPassword = (data: RecoverPasswordRequest) =>
+    apiInstance.post<void>("/auth/recover-password", data);
 
-  const resetPassword = async (data: ResetPasswordRequest) => {
-    return api.post<void>("/auth/reset-password", data);
-  };
+  const resetPassword = (data: ResetPasswordRequest) =>
+    apiInstance.post<void>("/auth/reset-password", data);
 
-  const refreshToken = async (data: RefreshTokenRequest) => {
-    return api.post<AuthResponse>("/auth/refresh-token", data);
-  };
+  const refreshToken = (data: RefreshTokenRequest) =>
+    apiInstance.post<AuthResponse>("/auth/refresh-token", data);
 
   return {
     login,
