fb973e36b09e1df8020e3ce232d92dd25f2cdb90
dariomatias-dev <matiasdario75@gmail.com>
2026-01-02 14:46:43 -0300

    feat(api/src/main/java/com/dariomatias/my_commerce/seed/category/CategorySeed.java): diversified record generation
    - Implemented diversified generation of category records.

--- api/src/main/java/com/dariomatias/my_commerce/seed/category/CategorySeed.java
+++ api/src/main/java/com/dariomatias/my_commerce/seed/category/CategorySeed.java
@@ -4,8 +4,9 @@ import com.dariomatias.my_commerce.model.Category;
 import com.dariomatias.my_commerce.model.Store;
 import com.dariomatias.my_commerce.repository.CategoryRepository;
 import com.dariomatias.my_commerce.repository.StoreRepository;
-import org.springframework.stereotype.Component;
 import jakarta.transaction.Transactional;
+import org.springframework.stereotype.Component;
+
 import java.util.List;
 
 @Component
@@ -24,11 +25,19 @@ public class CategorySeed {
         List<Store> stores = storeRepository.findAll();
         if (stores.isEmpty()) return;
 
-        for (int i = 1; i <= 20; i++) {
-            Category category = new Category();
-            category.setName("Categoria " + i);
-            category.setStore(stores.get(i % stores.size()));
-            categoryRepository.save(category);
+        int categoryIndex = 1;
+
+        for (Store store : stores) {
+            int categoriesPerStore = 2 + (categoryIndex % 4);
+
+            for (int i = 0; i < categoriesPerStore; i++) {
+                Category category = new Category();
+                category.setName("Categoria " + categoryIndex);
+                category.setStore(store);
+
+                categoryRepository.save(category);
+                categoryIndex++;
+            }
         }
     }
 }
