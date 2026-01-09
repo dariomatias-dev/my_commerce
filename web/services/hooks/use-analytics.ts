60248a02683c212fbab55079a944b0df72f28938
dariomatias-dev <matiasdario75@gmail.com>
2026-01-09 12:54:10 -0300

    feat(web): added new endpoint
    - Created a request method to retrieve all customers of the authenticated subscriber.

--- web/services/hooks/use-analytics.ts
+++ web/services/hooks/use-analytics.ts
@@ -2,25 +2,39 @@
 
 import { useCallback } from "react";
 
-import { apiClient } from "@/services/api-client";
 import { ConversionRateResponse } from "@/@types/analytics/conversion-rate-response";
+import { UniqueCustomersResponse } from "@/@types/analytics/unique-customers-response";
 import { VisitorsPerHourResponse } from "@/@types/analytics/visitors-per-hour-response";
+import { apiClient } from "@/services/api-client";
 
 export const useAnalytics = () => {
   const getVisitorsPerHourByStoreId = useCallback(
     (storeId: string) =>
-      apiClient.get<Array<VisitorsPerHourResponse>>(`/analytics/store/${storeId}/stats/activity-log`),
+      apiClient.get<Array<VisitorsPerHourResponse>>(
+        `/analytics/store/${storeId}/stats/activity-log`
+      ),
     []
   );
 
   const getConversionRateByStoreId = useCallback(
     (storeId: string) =>
-      apiClient.get<ConversionRateResponse>(`/analytics/store/${storeId}/stats/conversion-rate`),
+      apiClient.get<ConversionRateResponse>(
+        `/analytics/store/${storeId}/stats/conversion-rate`
+      ),
+    []
+  );
+
+  const getUniqueCustomers = useCallback(
+    () =>
+      apiClient.get<UniqueCustomersResponse>(
+        `/analytics/me/stats/unique-customers`
+      ),
     []
   );
 
   return {
     getVisitorsPerHourByStoreId,
     getConversionRateByStoreId,
+    getUniqueCustomers,
   };
 };
