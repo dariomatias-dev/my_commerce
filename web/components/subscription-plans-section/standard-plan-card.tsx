fcdfc42537fea464cc54d8d882871342a74f596a
dariomatias-dev <matiasdario75@gmail.com>
2026-01-18 06:52:16 -0300

    feat(web/components/subscription-plans-section): added plan features listing
    - Refactored the card features element to list only the text from the plan features property.

--- web/components/subscription-plans-section/standard-plan-card.tsx
+++ web/components/subscription-plans-section/standard-plan-card.tsx
@@ -19,7 +19,12 @@ export const StandardPlanCard = ({
   isLoading,
   isActive,
 }: StandardPlanCardProps) => {
-  const featuresList = plan.features?.split(",").map((f) => f.trim()) || [];
+  const featuresList =
+    plan.features
+      ?.split(";")
+      .map((f) => f.trim())
+      .filter(Boolean) || [];
+
   const isBusiness = index === 2;
 
   return (
@@ -28,13 +33,16 @@ export const StandardPlanCard = ({
         <span className="text-[10px] font-black tracking-[0.3em] text-slate-400 uppercase">
           {index === 0 ? "Start" : "Enterprise"}
         </span>
+
         <h3 className="mt-2 text-3xl font-black tracking-tighter text-slate-950 uppercase italic">
           {plan.name}
         </h3>
+
         <div className="mt-6 flex items-baseline gap-1">
           <span className="text-5xl font-black tracking-tighter text-slate-950 italic">
             R$ {plan.price}
           </span>
+
           <span className="text-sm font-bold tracking-widest text-slate-400 uppercase">
             /mês
           </span>
@@ -42,26 +50,6 @@ export const StandardPlanCard = ({
       </div>
 
       <div className="mb-10 grow space-y-5">
-        <div className="flex items-center gap-3 text-sm font-bold text-slate-500 italic">
-          <CheckCircle2
-            size={18}
-            className="text-slate-300 transition-colors group-hover:text-indigo-600"
-          />
-          {plan.maxStores === -1
-            ? "Lojas Ilimitadas"
-            : `${plan.maxStores} Loja(s) Ativa(s)`}
-        </div>
-
-        <div className="flex items-center gap-3 text-sm font-bold text-slate-500 italic">
-          <CheckCircle2
-            size={18}
-            className="text-slate-300 transition-colors group-hover:text-indigo-600"
-          />
-          {plan.maxProducts === -1
-            ? "Produtos Ilimitados"
-            : `Até ${plan.maxProducts} Produtos`}
-        </div>
-
         {featuresList.map((feature, idx) => (
           <div
             key={idx}
