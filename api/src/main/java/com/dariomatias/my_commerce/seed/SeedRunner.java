9d77ad6ac9a96db9b9dd4cee5f1e17958bbc2a8b
dariomatias-dev <matiasdario75@gmail.com>
2026-01-24 18:23:10 -0300

    feat(api/src/main/java/com/dariomatias/my_commerce/seed/SeedRunner.java): added logs
    - Implemented logs for feedback.

--- api/src/main/java/com/dariomatias/my_commerce/seed/SeedRunner.java
+++ api/src/main/java/com/dariomatias/my_commerce/seed/SeedRunner.java
@@ -10,6 +10,8 @@ import com.dariomatias.my_commerce.seed.subscription_plan.SubscriptionPlanSeed;
 import com.dariomatias.my_commerce.seed.user.UserSeed;
 import com.dariomatias.my_commerce.seed.user_address.UserAddressSeed;
 import lombok.RequiredArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Component;
@@ -19,8 +21,9 @@ import org.springframework.stereotype.Component;
 @RequiredArgsConstructor
 public class SeedRunner implements CommandLineRunner {
 
-    private final DatabaseReset databaseReset;
+    private static final Logger log = LoggerFactory.getLogger(SeedRunner.class);
 
+    private final DatabaseReset databaseReset;
     private final AdminUserSeed adminUserSeed;
     private final UserSeed userSeed;
     private final SubscriptionPlanSeed subscriptionPlanSeed;
@@ -33,16 +36,46 @@ public class SeedRunner implements CommandLineRunner {
 
     @Override
     public void run(String... args) {
+        log.info("SEED_RUNNER | Iniciando reset do banco de dados");
         databaseReset.reset();
+        log.info("SEED_RUNNER | Reset do banco finalizado");
 
+        log.info("SEED_RUNNER | Iniciando AdminUserSeed");
         adminUserSeed.run();
+        log.info("SEED_RUNNER | Finalizada AdminUserSeed");
+
+        log.info("SEED_RUNNER | Iniciando UserSeed");
         userSeed.run();
+        log.info("SEED_RUNNER | Finalizada UserSeed");
+
+        log.info("SEED_RUNNER | Iniciando SubscriptionPlanSeed");
         subscriptionPlanSeed.run();
+        log.info("SEED_RUNNER | Finalizada SubscriptionPlanSeed");
+
+        log.info("SEED_RUNNER | Iniciando SubscriptionSeed");
         subscriptionSeed.run();
+        log.info("SEED_RUNNER | Finalizada SubscriptionSeed");
+
+        log.info("SEED_RUNNER | Iniciando StoreSeed");
         storeSeed.run();
+        log.info("SEED_RUNNER | Finalizada StoreSeed");
+
+        log.info("SEED_RUNNER | Iniciando CategorySeed");
         categorySeed.run();
+        log.info("SEED_RUNNER | Finalizada CategorySeed");
+
+        log.info("SEED_RUNNER | Iniciando ProductSeed");
         productSeed.run();
+        log.info("SEED_RUNNER | Finalizada ProductSeed");
+
+        log.info("SEED_RUNNER | Iniciando UserAddressSeed");
         userAddressSeed.run();
+        log.info("SEED_RUNNER | Finalizada UserAddressSeed");
+
+        log.info("SEED_RUNNER | Iniciando OrderSeed");
         orderSeed.run();
+        log.info("SEED_RUNNER | Finalizada OrderSeed");
+
+        log.info("SEED_RUNNER | Todas as seeds finalizadas");
     }
 }
