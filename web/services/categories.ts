6375ed802f8db60a414a7b06612eb5e55ddda5a4
dariomatias-dev <matiasdario75@gmail.com>
2026-05-17 14:37:44 -0300

    feat(web/services): created categories service
    - Developed service for category resources.

---
+++ web/services/categories.ts
@@ -0,0 +1,17 @@
+import { CategoryFilter } from "@/@types/category/category-filter";
+import { CategoryResponse } from "@/@types/category/category-response";
+import { PaginatedResponse } from "@/@types/paginated-response";
+import { internalApiClient } from "@/services/api-client";
+
+export const getAllCategories = (
+  filter?: CategoryFilter,
+  page = 0,
+  size = 10,
+) =>
+  internalApiClient.get<PaginatedResponse<CategoryResponse>>(
+    "/api/categories",
+    { params: { ...filter, page, size } },
+  );
+
+export const getCategoryById = (id: string) =>
+  internalApiClient.get<CategoryResponse>(`/api/categories/${id}`);
