961df14464f14e8a3e0df68ef634a30fcfd8081a
dariomatias-dev <matiasdario75@gmail.com>
2026-05-17 09:37:45 -0300

    feat(web/app): created requests for user endpoints
    - Developed requests for the internal user endpoints.

---
+++ web/app/api/users/[id]/route.ts
@@ -0,0 +1,13 @@
+import { NextRequest, NextResponse } from "next/server";
+
+import { serverApi } from "@/lib/server-api";
+
+export async function GET(
+  _: NextRequest,
+  { params }: { params: Promise<{ id: string }> }
+) {
+  const { id } = await params;
+  const res = await serverApi.get(`/users/${id}`);
+  const data = await res.json();
+  return NextResponse.json(data, { status: res.status });
+}
---
+++ web/app/api/users/me/route.ts
@@ -0,0 +1,9 @@
+import { NextResponse } from "next/server";
+
+import { serverApi } from "@/lib/server-api";
+
+export async function GET() {
+  const res = await serverApi.get("/users/me");
+  const data = await res.json();
+  return NextResponse.json(data, { status: res.status });
+}
