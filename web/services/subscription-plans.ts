d650257f8b992625965dd024cc163a8f31cd1854
dariomatias-dev <matiasdario75@gmail.com>
2026-05-17 14:06:16 -0300

    feat(web/services): created subscription and subscription plan services
    - Developed services for subscription and subscription plan resources.

---
+++ web/services/subscription-plans.ts
@@ -0,0 +1,14 @@
+import { PaginatedResponse } from "@/@types/paginated-response";
+import { SubscriptionPlanResponse } from "@/@types/subscription-plan/subscription-plan-response";
+import { internalApiClient } from "@/services/api-client";
+
+export const getAllPlans = (page = 0, size = 10) =>
+  internalApiClient.get<PaginatedResponse<SubscriptionPlanResponse>>(
+    "/api/subscription-plans",
+    { params: { page, size } },
+  );
+
+export const getPlanById = (id: string) =>
+  internalApiClient.get<SubscriptionPlanResponse>(
+    `/api/subscription-plans/${id}`,
+  );
---
+++ web/services/subscriptions.ts
@@ -0,0 +1,31 @@
+import { PaginatedResponse } from "@/@types/paginated-response";
+import { SubscriptionResponse } from "@/@types/subscription/subscription-response";
+import { internalApiClient } from "@/services/api-client";
+
+export const getAllSubscriptions = (page = 0, size = 10) =>
+  internalApiClient.get<PaginatedResponse<SubscriptionResponse>>(
+    "/api/subscriptions",
+    { params: { page, size } },
+  );
+
+export const getMySubscriptions = (page = 0, size = 10) =>
+  internalApiClient.get<PaginatedResponse<SubscriptionResponse>>(
+    "/api/subscriptions/user/me",
+    { params: { page, size } },
+  );
+
+export const getSubscriptionsByUserId = (
+  userId: string,
+  page = 0,
+  size = 10,
+) =>
+  internalApiClient.get<PaginatedResponse<SubscriptionResponse>>(
+    `/api/subscriptions/user/${userId}`,
+    { params: { page, size } },
+  );
+
+export const getMyActiveSubscription = () =>
+  internalApiClient.get<SubscriptionResponse>("/api/subscriptions/me/active");
+
+export const getSubscriptionById = (id: string) =>
+  internalApiClient.get<SubscriptionResponse>(`/api/subscriptions/${id}`);
