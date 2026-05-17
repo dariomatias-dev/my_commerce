183b54c57c79f0083056179359e023109a1dd61c
dariomatias-dev <matiasdario75@gmail.com>
2026-05-17 10:21:23 -0300

    feat(web/services): created store service
    - Developed a service for store resources.

---
+++ web/services/stores.ts
@@ -0,0 +1,27 @@
+import { PaginatedResponse } from "@/@types/paginated-response";
+import { StoreFilter } from "@/@types/store/store-filter";
+import { StoreResponse } from "@/@types/store/store-response";
+import { internalApiClient } from "@/services/api-client";
+
+export const getAllByUser = (
+  filters: StoreFilter,
+  page = 0,
+  size = 10,
+) =>
+  internalApiClient.get<PaginatedResponse<StoreResponse>>("/api/stores", {
+    params: { ...filters, page, size },
+  });
+
+export const getMyStores = (page = 0, size = 10) =>
+  internalApiClient.get<PaginatedResponse<StoreResponse>>("/api/stores/me", {
+    params: { page, size },
+  });
+
+export const getStoreById = (id: string) =>
+  internalApiClient.get<StoreResponse>(`/api/stores/${id}`);
+
+export const getStoreBySlug = (slug: string) =>
+  internalApiClient.get<StoreResponse>(`/api/stores/slug/${slug}`);
+
+export const getTotalActiveStores = () =>
+  internalApiClient.get<number>("/api/stores/active/count");
