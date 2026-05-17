11f93a47df2e8fee6b6dfa0740ed947c7ee326f7
dariomatias-dev <matiasdario75@gmail.com>
2026-05-17 14:24:19 -0300

    feat(web/app/api): created requests for analytics endpoints
    - Developed requests for the internal analytics endpoints.

---
+++ web/app/api/analytics/me/stats/unique-customers/route.ts
@@ -0,0 +1,10 @@
+import { NextResponse } from "next/server";
+
+import { serverApi } from "@/lib/server-api";
+
+export async function GET() {
+  const res = await serverApi.get("/analytics/me/stats/unique-customers");
+  const data = await res.json();
+
+  return NextResponse.json(data, { status: res.status });
+}
