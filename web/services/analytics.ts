8cfb63f1f2fab32d828b11415f69499811300672
dariomatias-dev <matiasdario75@gmail.com>
2026-05-17 14:23:56 -0300

    feat(web/services): created analytics service
    - Developed a service for analytics resources.

---
+++ web/services/analytics.ts
@@ -0,0 +1,26 @@
+import { TotalRevenueResponse } from "@/@types/analytics/total-revenue-response";
+import { UniqueCustomersResponse } from "@/@types/analytics/unique-customers-response";
+import { internalApiClient } from "@/services/api-client";
+
+export const getUniqueCustomers = () =>
+  internalApiClient.get<UniqueCustomersResponse>(
+    "/api/analytics/me/stats/unique-customers",
+  );
+
+export const getMyTotalRevenue = () =>
+  internalApiClient.get<TotalRevenueResponse>(
+    "/api/analytics/me/stats/total-revenue",
+  );
+
+export const getUniqueCustomersByStoreId = (storeId: string) =>
+  internalApiClient.get<UniqueCustomersResponse>(
+    `/api/analytics/store/${storeId}/stats/unique-customers`,
+  );
+
+export const getTotalRevenueByStoreId = (storeId: string) =>
+  internalApiClient.get<TotalRevenueResponse>(
+    `/api/analytics/store/${storeId}/stats/total-revenue`,
+  );
+
+export const getTotalRevenue = () =>
+  internalApiClient.get<TotalRevenueResponse>("/api/analytics/total-revenue");
